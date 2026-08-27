c604a0ee55ddc561a01758fea3920aaacd346411
TulskaT <ttulska@gmail.com>
2026-08-27 23:36:42 +0100

    add change in files

---
+++ src/js/2-form.js
@@ -0,0 +1,46 @@
+const form = document.querySelector(".feedback-form");
+
+const formData = {
+  email: "",
+  message: "",
+};
+
+const STORAGE_KEY = "feedback-form-state";
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener("input", event => {
+  const { name, value } = event.target;
+
+  formData[name] = value.trim();
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+
+  formData.email = "";
+  formData.message = "";
+
+  form.reset();
+});
